82095c975e9ef51259a2d7f1f79a21ce3bf302a0
kormpakis <42711392+kormpakis@users.noreply.github.com>
2018-08-26 13:17:28 +0300

    Updated file test_plugin.php

--- test-plugin.php
+++ test-plugin.php
@@ -31,10 +31,6 @@
 				$mysqli2 = new mysqli( "localhost", "root", "", "wp" );
 				$recent_posts = wp_get_recent_posts( array( 'numberposts' => '1' ) );
 				$thePostID = $recent_posts[0]['ID'];
-			
-				//query2 apla kai mono gia na elegxw an trexei
-				//$query2 = $mysqli2->query("INSERT INTO interest_array (user_id, tags_id, count_tags) VALUES (1, 15, '$thePostID')");
-				//$mysqli2->close();
 				
 				//Antlhsh tags toy teleytaioy dhmosieymenoy post apo to ID poy vrethike parapanw
 				$mysqli3 = new mysqli( "localhost", "root", "", "wp" );
@@ -43,75 +39,44 @@
 
 				foreach ($tags_ids as &$value1) {
 					$the_tags_of_new_post = intval($value1);
-									
-					//query3 apla kai mono gia na elegxw an trexei
-					//$query3 = $mysqli3->query("INSERT INTO interest_array (user_id, tags_id, count_tags) VALUES (1, 300, '$the_tags_of_new_post')");
 				}
-				//$mysqli3->close();
 				
 				//Kathgoriopoihsh endiaferontos vasei tags, ana xrhsth
 				while($row = $ids->fetch_assoc()) {
 					$r_id = $row["ID"];
 					$sql_order_top = "SELECT tags_id FROM interest_array WHERE user_id='$r_id' ORDER BY count_tags DESC LIMIT 3";
 					$sql_order_middle = "SELECT tags_id FROM interest_array WHERE user_id='$r_id' ORDER BY count_tags DESC LIMIT 4,6";
+					
 					$ordered_tags_per_user_top = $mysqli->query($sql_order_top);
 					$top_priorities_arrays = mysqli_fetch_all($ordered_tags_per_user_top,MYSQLI_ASSOC);	
 					$top_priorities = array_column($top_priorities_arrays,'tags_id');
+					
 					$ordered_tags_per_user_middle = $mysqli->query($sql_order_middle);
 					$middle_priorities_arrays = mysqli_fetch_all($ordered_tags_per_user_middle,MYSQLI_ASSOC);
 					$middle_priorities = array_column($middle_priorities_arrays,'tags_id');
-					$result = array_intersect($top_priorities, $tags_ids);
-					$query7 = $mysqli->query("INSERT INTO interest_array (user_id, tags_id, count_tags) VALUES (1, 2, 3");
-
-					if(empty($result)){
-						
-						//$query4 = $mysqli->query("INSERT INTO interest_array (user_id, tags_id, count_tags) VALUES (444, 444, 444");
+					
+					$result = array_intersect($top_priorities, $tags_ids);					
+					if(empty($result)) {
+						$result2 = array_intersect($middle_priorities, $tags_ids);
+						if(empty($result2)) {
+							//Kamia eidopoihsh
+						}
+						else { //if(!empty($result2)) 
+							//Eidopoihsh gia priority 2
+						}
 					}
-					else {
+					else {//if(!empty($result1)){
+						//Eidopoihsh gia priority 1
+						//Testing code underneath
 						print_r('ok');
 						$mysqli8 = new mysqli( "localhost", "root", "", "wp" );
 						$sql_test = "INSERT INTO interest_array (user_id, tags_id, count_tags) VALUES (555, 444,444)";
 						$query5 = $mysqli8->query($sql_test);
 						$mysqli8->close();
 					}
-					//debug_to_console($middle_priorities);
 					}
 				$mysqli->close();	
-				
-								
-				//FOR OLOYS TOYS XRHSTES??????????
-				/*foreach ($ids as $value){
-					$result = array_intersect($top_priorities, $tags_ids);
 
-				
-				}*/
-				//////////////////////////////////////////////CHANGES
 			}
 		}
-		
-		
-// Pairnoyme to ID apo to pio prosfato post me skopo na to xrhsimopoihsoyme otan tha ginetai Publish ena neo arthro
-		/*function GetLastPostId($new_status, $old_status, $post){
-			if ( $new_status == 'publish' && $old_status != 'publish' ){		
-				$mysqli = new mysqli( "localhost", "root", "", "wp" );
-				$recent_posts = wp_get_recent_posts( array( 'numberposts' => '1' ) );
-				$thePostID = $recent_posts[0]['ID'];
-			
-				//query2 apla kai mono gia na elegxw an trexei
-				$query2 = $mysqli->query("INSERT INTO interest_array (user_id, tags_id, count_tags) VALUES (1, 15, '$thePostID')");
-				$mysqli->close();
-				
-				//ALLAGES APO DW KAI KATW
-				$mysqli2 = new mysqli( "localhost", "root", "", "wp" );
-				$tags_of_new_post = get_the_tags($thePostID);
-				$tags_ids = array_map(create_function('$o', 'return $o->term_id;'), $tags_of_new_post);	
-
-				foreach ($tags_ids as &$value) {
-					$the_tags_of_new_post = intval($value);
-					$query3 = $mysqli2->query("INSERT INTO interest_array (user_id, tags_id, count_tags) VALUES (1, 25, '$the_tags_of_new_post')");
-				}
-				$mysqli2->close();
-				//return $id;
-			}
-		}*/
 ?>
