e4383a652ba99c2e905b37e1e83a2731c6b3016f
Giorgos Kormpakis <42711392+kormpakis@users.noreply.github.com>
2018-09-06 21:09:57 +0300

    SPARQL_Queries - testing the ontology

---
+++ SPARQL_scripts.sql
@@ -0,0 +1,5 @@
+//Various SPARQL queries - testing the ontology
+
+SELECT ?x WHERE {?x rdfs:subClassOf<http://www.semanticweb.org/user/ontologies/2018/8/untitled-ontology-5#Genre>.}
+
+SELECT ?x WHERE {?x  rdf:type<http://www.semanticweb.org/user/ontologies/2018/8/untitled-ontology-5#User>.}
