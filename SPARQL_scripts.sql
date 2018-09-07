83aeccd83175d804b41c013a12bee8f12c59a182
Giorgos Kormpakis <42711392+kormpakis@users.noreply.github.com>
2018-09-07 13:07:22 +0300

    Some more queries
    Mainly checking the inverse object properties

--- SPARQL_scripts.sql
+++ SPARQL_scripts.sql
@@ -1,5 +1,17 @@
 //Various SPARQL queries - testing the ontology
 
-SELECT ?x WHERE {?x rdfs:subClassOf<http://www.semanticweb.org/user/ontologies/2018/8/untitled-ontology-5#Genre>.}
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+PREFIX : <http://www.semanticweb.org/user/ontologies/2018/8/untitled-ontology-5#>
 
-SELECT ?x WHERE {?x  rdf:type<http://www.semanticweb.org/user/ontologies/2018/8/untitled-ontology-5#User>.}
+SELECT ?x WHERE {?x :genre_liked_by :Korbakis.}
+
+SELECT ?nn ?mm WHERE {?nn :is_of_genre :heavy_metal.}
+
+SELECT ?x ?y WHERE {?x  :likes_genre ?y.}
+
+SELECT ?x WHERE {?x rdfs:subClassOf :Genre.}
+
+SELECT ?x WHERE {?x  rdf:type :User.}
