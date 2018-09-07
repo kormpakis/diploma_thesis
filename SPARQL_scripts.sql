5492d4cd5f00bad9d2cce3c240c6c2f5ab52b5d0
Giorgos Kormpakis <42711392+kormpakis@users.noreply.github.com>
2018-09-07 14:07:53 +0300

    Just further testing

--- SPARQL_scripts.sql
+++ SPARQL_scripts.sql
@@ -6,12 +6,17 @@ PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 PREFIX : <http://www.semanticweb.org/user/ontologies/2018/8/untitled-ontology-5#>
 
-SELECT ?x WHERE {?x :genre_liked_by :Korbakis.}
+SELECT ?x WHERE { ?x :genre_liked_by :Korbakis. }
 
-SELECT ?nn ?mm WHERE {?nn :is_of_genre :heavy_metal.}
+SELECT ?nn ?mm WHERE { ?nn :is_of_genre :heavy_metal. }
 
-SELECT ?x ?y WHERE {?x  :likes_genre ?y.}
+SELECT ?x ?y WHERE { ?x :likes_genre ?y. }
 
-SELECT ?x WHERE {?x rdfs:subClassOf :Genre.}
+SELECT ?x WHERE { ?x rdfs:subClassOf :Genre. }
 
-SELECT ?x WHERE {?x  rdf:type :User.}
+SELECT ?x WHERE { ?x  rdf:type :User. }
+
+SELECT ?x ?y WHERE { ?x rdf:type :User.
+					?x :Birth_date ?y }
+
+SELECT ?x WHERE { :Korbakis :Birth_date ?x. }
